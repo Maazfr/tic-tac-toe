01161545b5a41f764fbd71ab8f92809afe33a1d1
Maazfr <146356081+Maazfr@users.noreply.github.com>
2024-11-18 23:08:00 +0530

    ttc.c

--- ttc.c
+++ ttc.c
@@ -4,19 +4,25 @@ char board[3][3]; // 3x3 Tic-Tac-Toe board
 char currentPlayer = 'X'; // Starting player
 
 // Function to initialize the board with empty spaces
-void initializeBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+void initializeBoard() 
+{
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
             board[i][j] = ' ';
         }
     }
 }
 
 // Function to print the board
-void printBoard() {
+void printBoard() 
+{
     printf("\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++)
+            {
             printf(" %c ", board[i][j]);
             if (j < 2) printf("|");
         }
@@ -29,24 +35,30 @@ void printBoard() {
 // Function to check if the current player has won
 int checkWin() {
     // Check rows
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+    for (int i = 0; i < 3; i++) 
+    {
+        if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) 
+        {
             return 1;
         }
     }
 
     // Check columns
-    for (int i = 0; i < 3; i++) {
-        if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+    for (int i = 0; i < 3; i++) 
+    {
+        if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) 
+        {
             return 1;
         }
     }
 
     // Check diagonals
-    if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+    if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer)
+    {
         return 1;
     }
-    if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+    if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)
+    {
         return 1;
     }
 
@@ -55,9 +67,12 @@ int checkWin() {
 
 // Function to check if the board is full
 int isBoardFull() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
+            if (board[i][j] == ' ') 
+            {
                 return 0; // If there's an empty space
             }
         }
@@ -67,15 +82,18 @@ int isBoardFull() {
 
 // Function to switch players
 void switchPlayer() {
-    if (currentPlayer == 'X') {
+    if (currentPlayer == 'X') 
+    {
         currentPlayer = 'O';
-    } else {
+    } else 
+    {
         currentPlayer = 'X';
     }
 }
 
 // Function to play the game
-void playGame() {
+void playGame() 
+{
     int row, col;
     int gameWon = 0;
 
@@ -91,26 +109,34 @@ void playGame() {
         col--;
 
         // Check if the position is valid
-        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') 
+        {
+            
             // Make the move
             board[row][col] = currentPlayer;
 
             // Check if the current player has won
-            if (checkWin()) {
+            if (checkWin()) 
+            {
                 gameWon = 1;
                 printBoard();
                 printf("Player %c wins!\n", currentPlayer);
-            } else {
+            } 
+            else 
+            {
                 // Switch players
                 switchPlayer();
             }
-        } else {
+        } 
+        else 
+        {
             printf("Invalid move, try again.\n");
         }
     }
 
     // If the board is full and no one has won, it's a draw
-    if (!gameWon) {
+    if (!gameWon) 
+    {
         printBoard();
         printf("It's a draw!\n");
     }
